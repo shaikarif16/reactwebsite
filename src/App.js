0c92a9890c73a66d3a224384573a72783e14d599
Arif Shaik <shaikarif00207@gmal.com>
2020-09-17 17:18:44 +0530

    my first commit react website

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Service from "./components/Service";
+import Footer from "./components/Footer";
+
+import { Switch, Route, Redirect } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/services" component={Service} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Redirect to="/" />
+      </Switch>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
